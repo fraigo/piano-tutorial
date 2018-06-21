71641851d40ba2b3d4f82887fc448e1752f890a0
Francisco Igor <franciscoigor@gmail.com>
2018-06-20 22:31:46 -0700

    fixed scrolling

--- js/components/chapterHeader.js
+++ js/components/chapterHeader.js
@@ -2,8 +2,7 @@
 Vue.component("chapterHeader",{
     props:{
         prev:String,
-        next:String,
-        current:String
+        next:String
     },
     created:function(){
     },
@@ -14,7 +13,6 @@ Vue.component("chapterHeader",{
     },
     template:`<center style="position:sticky; top:-1px; z-index:10000; background-color:rgba(255,255,255,0.9)">
     <hr/>
-    <a :id="current"></a>
     <table width="100%"><tr>
     <td width=20 valign="center"><button v-if="prev" @click="goTo(prev)">Prev</button></td>
     <td  valign="center"><h2 style="margin:1px"><slot></slot></h2></td>
